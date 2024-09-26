b59e38ca4995b336f897b35aa5bcff9bf7c4e836
Yağız Arda Furtun <101671681+YAF61@users.noreply.github.com>
2024-09-26 23:16:59 +0300

    Add files via upload

---
+++ discord-profile.js
@@ -0,0 +1,24 @@
+fetch('https://api.lanyard.rest/v1/users/902170576574115890')
+  .then(response => response.json())
+  .then(data => {
+    const avatarUrl = `https://cdn.discordapp.com/avatars/${data.data.discord_user.id}/${data.data.discord_user.avatar}.png`;
+    const username = data.data.discord_user.username;
+    const status = data.data.discord_status ? data.data.discord_status : 'offline';
+
+    // İngilizce durumları Türkçe'ye çevirme
+    const turkceDurumlar = {
+        online: "Çevrimiçi",
+        idle: "Boşta",
+        dnd: "Rahatsız Etme",
+        offline: "Çevrimdışı"
+    };
+
+    // API'den gelen İngilizce durumu Türkçe'ye çevirme
+    const turkceDurum = turkceDurumlar[status] || "Bilinmiyor";
+
+    // Durumu HTML içine yerleştirme
+    document.getElementById('discord-avatar').src = avatarUrl;
+    document.getElementById('discord-username').textContent = username;
+    document.getElementById('discord-status-text').textContent = `Durum: ${turkceDurum}`;
+})
+.catch(error => console.error('Error fetching Lanyard data:', error));
